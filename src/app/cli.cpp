35121af1a44868b21c2fc677e50bc1d501647b3d
Jakob Ketterl <jakob.ketterl@gmx.de>
2021-05-20 11:01:15 +0200

    create codec-cli

---
+++ src/app/cli.cpp
@@ -0,0 +1,9 @@
+#include "cli.hpp"
+#include <iostream>
+
+using namespace CodecServer;
+
+int Cli::main(int argc, char** argv) {
+    std::cout << "CLI running\n";
+    return 0;
+}
---
+++ src/app/cli.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+namespace CodecServer {
+
+    class Cli {
+        public:
+            int main(int argc, char** argv);
+    };
+
+}
